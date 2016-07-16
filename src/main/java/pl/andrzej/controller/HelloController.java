5531047686325e1b62af0bbde1f48a4723826152
Andrzej <andrzej@wp.pl>
2016-07-16 22:03:41 +0200

    modelAndView

--- src/main/java/pl/andrzej/controller/HelloController.java
+++ src/main/java/pl/andrzej/controller/HelloController.java
@@ -5,19 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Andrzej on 16.07.2016.
  */
 @Controller
-@RequestMapping(value="/hello")
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap modelMap) {
 
         modelMap.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
 
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public ModelAndView printHello2(ModelAndView modelAndView) {
+        modelAndView.addObject("message", "Hello2");
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
 }
